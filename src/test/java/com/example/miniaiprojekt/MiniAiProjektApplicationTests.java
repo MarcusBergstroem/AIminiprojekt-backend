9d0392587d0330e73066cfb0eff1e30b8ca6ee0e
Ali <alku0001@stud.kea.dk>
2025-04-07 09:37:41 +0200

    Første upload.

---
+++ src/test/java/com/example/miniaiprojekt/MiniAiProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniaiprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAiProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
