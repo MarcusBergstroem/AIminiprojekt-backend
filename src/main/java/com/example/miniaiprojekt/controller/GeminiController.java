0ff3c694e728cfdb5c67a14084406046d31305e7
Marcus <marcus.bstroem@gmail.com>
2025-04-11 12:20:53 +0200

    Virker nok ikke, men med securitas:))

--- src/main/java/com/example/miniaiprojekt/controller/GeminiController.java
+++ src/main/java/com/example/miniaiprojekt/controller/GeminiController.java
@@ -2,13 +2,12 @@ package com.example.miniaiprojekt.controller;
 
 import com.example.miniaiprojekt.service.GeminiService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/api/gemini")
+@CrossOrigin("http://localhost:63342/")
 public class GeminiController {
 
     private final GeminiService geminiService;
@@ -17,17 +16,13 @@ public class GeminiController {
         this.geminiService = geminiService;
     }
 
-    @GetMapping("/")
-    public String index() {
-        return "Hej med dig";
-    }
-
     @GetMapping("/bookhelper")
     public String bookkeeper() {
-        return "/bookhelper.html";
+        return "bookhelper";
     }
 
     @GetMapping("/generate")
+    @ResponseBody
     // Requestparam er den inputtekst som brugeren har skrevet i API-kaldet
     public ResponseEntity<String> generate(@RequestParam String prompt) {
         // Block her betyder at man venter på svaret fra API-kaldet
